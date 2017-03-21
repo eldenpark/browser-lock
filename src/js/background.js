b4cf079d59632bc43f363a229c818e217393868b
haneek <hwanhee.kim@navercorp.com>
2017-03-21 21:12:41 +0900

    Modify background.js and add lock.html

--- src/js/background.js
+++ src/js/background.js
@@ -1,12 +1,41 @@
 function closeAllTab() {
   console.log("closeAllTab()");
 
-  chrome.tabs.getAllInWindow(function (tabs) {
-    for(var i = 0; i <  tabs.length; i++) {
-      chrome.tabs.remove(tabs[i].id, function () { });
-      console.log(tabs[i].url);
-    }
+  var lockPageTabId;
+
+  // pattern lock page open
+  chrome.tabs.create({"url":"/html/lock.html"}, function(tab) {
+    console.log("lock page tab create");
+    lockPageTabId = tab.id;
   });
 
-  chrome.tabs.create("www.naver.com");
+  // close all tab (save tab url)
+  chrome.tabs.getAllInWindow(function (tabs) {
+    var pastUrls = new Array();
+
+    for(var i = 0; i <  tabs.length; i++) {
+      if(tabs[i].id != lockPageTabId) {
+        chrome.tabs.remove(tabs[i].id, function () { });
+        pastUrls.push(tabs[i].url);
+      }
+    }
+    
+    saveTabUrlIntoStorage(pastUrls);
+  });
+
+  
+
+  // 일주일까지의 기록 삭제
+}
+
+function saveTabUrlIntoStorage(pastUrls) {
+
+  var saveUrlFormat = "{";
+  for(var i = 0; i < pastUrls.length; i++) {
+    saveUrlFormat += pastUrls[i] + ";"
+  }
+  saveUrlFormat += "}";
+
+  console.log(saveUrlFormat);
+  
 }
