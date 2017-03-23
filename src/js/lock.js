97b350dbb6d7bfa36ece5bbb29d50056e092199a
haneek <hwanhee.kim@navercorp.com>
2017-03-24 04:52:15 +0900

    Modify lock.js

--- src/js/lock.js
+++ src/js/lock.js
@@ -1,9 +1,30 @@
 import patternLock from './modules/patternLock'
 import pwConf from './modules/pwConf'
 import background from './background'
+import handleLockStatus from './modules/handleLockStatus'
+
+let browserIsLocked;
 
 window.onload = () => {
+  let checkStatus = handleLockStatus.checkLockStatus(getLockStatusSuccessCallBack);
+
+  Promise
+    .all([checkStatus])
+    .then(() => {
+      console.log("check lock : " + browserIsLocked);
+      if (browserIsLocked == false) {
+        location.href = "http://google.co.kr";
+      }
+    })
 
   patternLock.promptReady(background.openAllPastUrls);
 
+}
+
+function getLockStatusSuccessCallBack(status) {
+  if (status == 'lock') {
+    browserIsLocked = true;
+  } else {
+    browserIsLocked = false;
+  }
 }
